f8e7b57fbe20f2cca27a403e2242c8edc0c82ea8
csirota97 <csirota97@gmail.com>
2024-02-19 12:16:36 -0500

    Added unit tests, removed unused numpy

--- src/prediction/predictor.py
+++ src/prediction/predictor.py
@@ -1,6 +1,5 @@
 import json
 import nltk
-import numpy
 import os
 import pickle
 import typing
@@ -8,10 +7,26 @@ import typing
 
 class Predictor:
     '''
-    Module for determining which predefined message the provided query is asking.
+    Module for determining which predefined message the provided query is
+    asking.
     '''
-    def __init__(self, intents_filename: str):
-        '''Takes in JSON filename of predefined messages the user could be asking.
+    def __init__(
+        self,
+        intents_filename: str,
+        pickle_file: str = "data.pickle",
+        intents_cache_file: str = "intents_cache"
+    ):
+        '''
+        Takes in JSON filename of predefined messages the user could be asking, filename for pickled data, and a
+            filename for the intents cache.
+
+        pickle_file defaults to "data.pickle" and is used to save the trained model to a serialized object
+        intents_cache_file defaults to "intents_cache" and is used to detect any changes between the intents file and
+            whatever intents file was previously used for training the.
+
+        If the contents of the intents file and the intents cache file are the same, the model will load the trained
+            from the pickle_file; otherwise, the model will be trained on the contents of the intents file, copy that
+            content to the intents_cache_file, and serialize the trained output to the data.pickle file.
 
         The json hould be in the following format
         {"intents: [
@@ -30,14 +45,24 @@ class Predictor:
         {"intents: [
             {
                 "tag":"lights_on",
-                "patterns": ["turn on the lights", "it is too dark in here", "I want the lights on", "I want it brighter in here"],
+                "patterns": [
+                    "turn on the lights",
+                    "it is too dark in here",
+                    "I want the lights on",
+                    "I want it brighter in here"
+                ],
                 "responses":4,
                 "context_set":"",
                 "flags": {"1": 4}
             },
             {
                 "tag":"lights_off",
-                "patterns": ["turn off the lights", "it is too bright in here", "I want the lights off", "I want it darker in here"],
+                "patterns": [
+                    "turn off the lights",
+                    "it is too bright in here",
+                    "I want the lights off",
+                    "I want it darker in here"
+                ],
                 "responses":5,
                 "context_set":"",
                 "flags": false
@@ -46,49 +71,44 @@ class Predictor:
 
         '''
         os.makedirs(".PredictorCache", exist_ok=True)
-
-        cached_intents = ""
+        self.pickle_file = pickle_file
+        self.intents_cache_file = intents_cache_file
 
         self.stemmer = nltk.stem.LancasterStemmer()
 
         try:
-            with open(os.path.join(".PredictorCache", "intents_cache"), "r") as cached_intents_file:
+            with open(os.path.join(".PredictorCache", self.intents_cache_file), "r") as cached_intents_file:
                 cached_intents = cached_intents_file.read()
         except FileNotFoundError:
-            pass
+            cached_intents = ""
 
+        with open(intents_filename, "r") as intents_file:
+            intents = intents_file.read()
+
+        with open(intents_filename, "rb") as intents_file_bytes:
+            self.intents_json = json.load(intents_file_bytes)
+
+        open_pickled_data = False
         try:
-            with open(intents_filename, "r") as intents_file:
-                intents = intents_file.read()
+            with open(os.path.join(".PredictorCache", self.pickle_file), "rb") as file_bytes:
+                self.words, self.labels, self.training, self.output = pickle.load(file_bytes)
 
-            with open(intents_filename, "rb") as intents_file_bytes:
-                self.intents_json = json.load(intents_file_bytes)
-
-            open_pickled_data = False
-            try:
-                with open(os.path.join(".PredictorCache", "data.pickle"), "rb") as file_bytes:
-                    self.words, self.labels, self.training, self.output = pickle.load(file_bytes)
-
-                open_pickled_data = True
-            except FileNotFoundError:
-                pass
-
-            if cached_intents != intents or not open_pickled_data:
-                with open(intents_filename, "r") as intents_file:
-                    self.__train_model_from_intents(json.load(intents_file))
-
-                with open(os.path.join(".PredictorCache", "intents_cache"), "w") as ic:
-                    ic.write(intents)
+            open_pickled_data = True
         except FileNotFoundError:
             pass
+
+        if cached_intents != intents or not open_pickled_data:
+            with open(intents_filename, "r") as intents_file:
+                self.__train_model_from_intents(json.load(intents_file))
+
+            with open(os.path.join(".PredictorCache", self.intents_cache_file), "w") as ic:
+                ic.write(intents)
 
     def __train_model_from_intents(self, intents_json: typing.Dict[str, list[typing.Dict[str, str]]]):
         '''
         Private method to train data on new intents JSON file
         Takes in result of json.load(<Intents JSON file>)
         '''
-        print(intents_json)
-        help(intents_json)
         print("Training data")
         self.intents_json = intents_json
         words = []
@@ -133,15 +153,12 @@ class Predictor:
             training.append(bag)
             output.append(output_row)
 
-        training = numpy.array(training)
-        output = numpy.array(output)
-
         self.words = words
         self.labels = labels
         self.training = training
         self.output = output
 
-        with open(os.path.join(".PredictorCache", "data.pickle"), "wb") as f:
+        with open(os.path.join(".PredictorCache", self.pickle_file), "wb") as f:
             pickle.dump((words, labels, training, output), f)
 
     def query(self, query: str):
@@ -171,12 +188,15 @@ class Predictor:
                 lineWithMostPoints = index
                 maxPoints = points
 
-        output_list = output.tolist()
+        output_list = output
         final_output = []
         for output_row in output_list:
             if output_row not in final_output:
                 final_output.append(output_row)
 
         for index, line in enumerate(final_output):
-            if line == output[lineWithMostPoints].tolist():
-                return (self.intents_json['intents'][index+1]['responses'], self.intents_json['intents'][index+1]['flags'])
+            if line == output[lineWithMostPoints]:
+                return (
+                    self.intents_json['intents'][index+1]['responses'],
+                    self.intents_json['intents'][index+1]['flags']
+                )
